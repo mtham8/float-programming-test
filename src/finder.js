35b0075928952e1a18c658ffd831dc5bf63d8ebe
Michelle <mtham101@gmail.com>
2016-06-15 10:01:58 -0700

    [Complete] finder.js

--- src/finder.js
+++ src/finder.js
@@ -2,17 +2,24 @@
 
 /*
  * finder
- * 
+ *
  * Takes an input and a test function and returns any values in
  * the input that pass the test.
- * 
+ *
  * Eg:
- * 
+ *
  *   input: [ 'ant', 'baby', [ 'apple', 'banana', 'carrot' ], { foo: 'aardvark' }, 'allegory' ]
  *   test: value => /^a/i.test( value )
  *   returns: [ 'ant', 'allegory' ]
- * 
+ *
  */
 module.exports = ( input, test ) => {
-
+  let output = [];
+  for (let i = 0; i < input.length; i++) {
+    let currentVal = input[i]
+    if(test(currentVal) && typeof currentVal !== 'object') {
+      output.push(currentVal);
+    }
+  }
+  return output;
 };
